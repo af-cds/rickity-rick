8fbb104320405350e18fa748937614c5fb8fd0e4
af-cds <adrian.fricke@gmail.com>
2023-02-06 17:27:34 +0100

    added error handling

--- index.js
+++ index.js
@@ -23,34 +23,45 @@ fetchCharacters()
 
 async function fetchCharacters() {
 
-    let url =  `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`
-    const response = await fetch(url)
-    const data = await response.json()
+try {
+
+  let url =  `https://rickandmortyapi.com/api/character?page=${page}&name=${searchQuery}`
+  const response = await fetch(url)
+  const data = await response.json()
+  
+  maxPage = data.info.pages 
+  console.log(maxPage)
+
+  cardContainer.innerHTML = ''
+
+  pagination.innerHTML = `${page} / ${maxPage}`
+
+  const listOfCharacters = data.results
+  const specifiedCharacters = listOfCharacters.map(result =>
+   ({ src: result.image, 
+  name: result.name, 
+  status: result.status,
+  type: result.type,
+  occurance: result.episode.length }))
+    
+  specifiedCharacters.forEach(character => {
+    const newCharacter = createCharacterCard(character)
+     cardContainer.append(newCharacter)
+    })  
+  
+
+} catch (error) {
+  cardContainer.innerHTML = "<img src=\'https://imgs.search.brave.com/8NjI8SThttpk4cM3XEJeTFpQ50YLoDi1bcbZ-_wgfr4/rs:fit:891:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4z/UVFYVTl0UXljcmRV/MDJiNHc5X2V3SGFE/OCZwaWQ9QXBp'>" + "No results found! Please try again"
+  console.error("you dun goofed up with harry") 
+  pagination.innerHTML = ""
+  navigation.innerHTML = ""
+
+ }
+}
+
 
     // data needed - the src of the image, the name of the character, the status, type and occurrences values
 
-    maxPage = data.info.pages 
-    console.log(maxPage)
-
-    cardContainer.innerHTML = ''
-
-    pagination.innerHTML = `${page} / ${maxPage}`
-
-    const listOfCharacters = data.results
-    const specifiedCharacters = listOfCharacters.map(result =>
-     ({ src: result.image, 
-    name: result.name, 
-    status: result.status,
-    type: result.type,
-    occurance: result.episode.length }))
-      
-    console.log(specifiedCharacters)
-
-    specifiedCharacters.forEach(character => {
-      const newCharacter = createCharacterCard(character)
-       cardContainer.append(newCharacter)
-      })  
-    }
 
     nextButton.addEventListener('click', (event) => {
      if (page < maxPage) { page ++
@@ -68,15 +79,6 @@ async function fetchCharacters() {
 
 
 // update pagination element
-
-// searchBar.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const searchBarData = new FormData(event.target);
-//   const data = Object.fromEntries(searchBarData);
-//   searchQuery = data.query;
-//   fetchCharacters()
-//   searchBar.reset();
-// });
 
 searchBar.addEventListener("submit", (event) => {
   
